f58cdd4d7746ac99633bde217d3d3256f8b06763
Tomoki Tamura <tomoki027.7939@icloud.com>
2023-12-27 16:32:04 +0900

    airlinesテーブル完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_070554) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_27_072820) do
   create_table "administrators", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "login_name", null: false
+    t.string "password", null: false
+    t.string "password_confirmation", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "airlines", force: :cascade do |t|
     t.string "name", null: false
     t.string "login_name", null: false
     t.string "password", null: false
---
+++ db/seeds/development/airlines.rb
@@ -0,0 +1,17 @@
+Airline.create(name: "JAL",
+               login_name: "jal",
+               password: "line",
+               password_confirmation: "line"
+)
+
+Airline.create(name: "ANA",
+               login_name: "ana",
+               password: "line",
+               password_confirmation: "line"
+)
+
+Airline.create(name: "PEACH",
+               login_name: "peach",
+               password: "line",
+               password_confirmation: "line"
+)
