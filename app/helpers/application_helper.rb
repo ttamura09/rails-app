6c15510896d5b9277cc1ce5dc153abcd618ad472
Tomoki Tamura <tomoki027.7939@icloud.com>
2024-01-04 00:02:49 +0900

    モックアップ完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def page_title
+    title = "BlueTicket"
+    title = @page_title + "-" + title if @page_title
+    title
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_27_080333) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_03_145646) do
   create_table "administrators", force: :cascade do |t|
     t.string "name", null: false
     t.string "login_name", null: false
-    t.string "password", null: false
-    t.string "password_confirmation", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,8 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_080333) do
   create_table "airlines", force: :cascade do |t|
     t.string "name", null: false
     t.string "login_name", null: false
-    t.string "password", null: false
-    t.string "password_confirmation", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -44,17 +42,60 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_27_080333) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "booking_seat_flights", force: :cascade do |t|
+    t.integer "booking_id"
+    t.integer "seat_id"
+    t.integer "flight_id"
+    t.boolean "checkin", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_booking_seat_flights_on_booking_id"
+    t.index ["flight_id"], name: "index_booking_seat_flights_on_flight_id"
+    t.index ["seat_id"], name: "index_booking_seat_flights_on_seat_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "flight_id"
+    t.integer "booking_seat_flight_id"
+    t.integer "total_price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_seat_flight_id"], name: "index_bookings_on_booking_seat_flight_id"
+    t.index ["customer_id"], name: "index_bookings_on_customer_id"
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "name", null: false
     t.string "alph_name", null: false
     t.string "login_name", null: false
     t.integer "sex", default: 1, null: false
     t.date "birthday", null: false
-    t.string "password", null: false
-    t.string "password_confirmation", null: false
+    t.string "password_digest", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "flights", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "airline_id", null: false
+    t.integer "airmodel_id", null: false
+    t.date "departure_date", null: false
+    t.time "departure_time", null: false
+    t.date "arrival_date", null: false
+    t.time "arrival_time", null: false
+    t.integer "origin_id", null: false
+    t.integer "destination_id", null: false
+    t.boolean "operation", default: true, null: false
+    t.integer "price", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
+    t.index ["airmodel_id"], name: "index_flights_on_airmodel_id"
+    t.index ["destination_id"], name: "index_flights_on_destination_id"
+    t.index ["origin_id"], name: "index_flights_on_origin_id"
   end
 
   create_table "seats", force: :cascade do |t|
