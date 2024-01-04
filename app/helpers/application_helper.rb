7d03fdec2bdbc90a9fa184c03b8af754a081e4fd
Tomoki Tamura <tomoki027.7939@icloud.com>
2024-01-04 15:48:53 +0900

    メニューバー,CSS完了(簡易的)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     title = @page_title + "-" + title if @page_title
     title
   end
+
+  def menu_link_to(text, path, options = {})
+    content_tag :li do
+      condition = options[:method] || !current_page?(path)
+
+      link_to_if(condition, text, path, options) do
+        content_tag(:span, text)
+      end
+    end
+  end
 end
