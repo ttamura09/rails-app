c25c1544b431d59864befa6f770a270ef46fac8c
Tomoki Tamura <tomoki027.7939@icloud.com>
2024-01-06 01:12:45 +0900

    table.css適用

--- db/seeds/development/bookings.rb
+++ db/seeds/development/bookings.rb
@@ -2,7 +2,7 @@ Booking.create(
   customer_id: 5,
   flight_id: 1393,
   booking_seat_flight_id: nil,
-  total_price: 7000,
+  total_price: 15000,
   passenger1_name: "松永賢次",
   passenger1_birthday: "	1981-12-01",
   passenger1_email: "matsunaga@example.com",
